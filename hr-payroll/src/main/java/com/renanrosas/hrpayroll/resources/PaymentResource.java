fdb06d875b3578c0426170207a8de9231ef5447a
Renan Rosas <renan-rosas@hotmail.com>
2021-08-13 23:22:23 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/renanrosas/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/renanrosas/hrpayroll/resources/PaymentResource.java
@@ -1,5 +1,6 @@
 package com.renanrosas.hrpayroll.resources;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.renanrosas.hrpayroll.entities.Payment;
 import com.renanrosas.hrpayroll.services.PaymentService;
 
@@ -17,10 +18,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
     }
 
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+    	Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }
 }
